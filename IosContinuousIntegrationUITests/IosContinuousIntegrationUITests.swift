093b906390ff2028c5b4471933361999c2de6b70
Adrien Nicolet <adrien.nicolet@zuehlke.com>
2015-10-19 09:00:00 +0200

    add example dummy unit and ui tests

--- IosContinuousIntegrationUITests/IosContinuousIntegrationUITests.swift
+++ IosContinuousIntegrationUITests/IosContinuousIntegrationUITests.swift
@@ -23,14 +23,21 @@ class IosContinuousIntegrationUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.navigationBars["Master"].buttons["Add"].tap()
+        let tablesQuery = app.tables
+        
+        XCTAssertEqual(tablesQuery.cells.count, 1)
+
+        let staticText = tablesQuery.cells.elementBoundByIndex(0)
+        staticText.tap()
+        app.navigationBars.matchingIdentifier("Detail").buttons["Master"].tap()
+        staticText.swipeLeft()
+        tablesQuery.buttons["Delete"].tap()
+        
+        XCTAssertEqual(tablesQuery.cells.count, 0)
     }
     
 }
